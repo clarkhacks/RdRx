c7ed9e8dc70f482c3f04becefbd7098fc7402087
Clark Weckmann <15677451+clarkhacks@users.noreply.github.com>
2025-09-22 21:14:51 -0500

    fix: Update asset links to use relative paths for icons and images

--- src/components/layouts/DocumentHead.ts
+++ src/components/layouts/DocumentHead.ts
@@ -48,7 +48,7 @@ function renderDocumentHead({ title, additionalScripts = '', noMeta = false, cus
   <link rel="apple-touch-icon" sizes="114x114" href="/assets/apple-icon-114x114.png">
   <link rel="apple-touch-icon" sizes="120x120" href="/assets/apple-icon-120x120.png">
   <link rel="apple-touch-icon" sizes="144x144" href="/assets/apple-icon-144x144.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/assets/apple-icon-152x152.png">
   <link rel="apple-touch-icon" sizes="180x180" href="/assets/apple-icon-180x180.png">
   <link rel="icon" type="image/png" sizes="192x192"  href="/assets/android-icon-192x192.png">
   <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon-32x32.png">
